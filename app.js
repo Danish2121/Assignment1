a9d038859a0fe3f8ff246b6978ca67b4c9032f64
Danish2121 <Danish2121@users.noreply.github.com>
2022-07-16 15:46:28 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,68 @@
+//  Chapter # 1
+
+
+//  Question 1 
+alert('Javascript');
+
+//  Question 2
+alert('Error! Please enter a valid password');
+
+//  Question 3
+alert('Welcome to JS Land \n Happy Coding!');
+
+//  Question 4
+alert('Welcome to JS Land');
+alert('Happy Coding!');
+
+//  Question 5
+console.log("Hello... I can run JS through my web browser's console");
+
+
+//  Chapter # 2
+
+//  Question 1
+var username;
+
+
+//  Question 2
+var myName = "Muhammad Danish";
+
+
+//  Question 3
+var message = "Hello World";
+alert(message);
+
+
+//  Question 4
+var name = "Muhammad Danish";
+var age = "22 Years Old";
+var course = "Web & Mobile App Development";
+alert(name);
+alert(age);
+alert(course);
+
+
+//  Question 5
+var food;
+alert('PIZZA \n PIZZ \n PIZ \n PI \n P');
+
+
+//  Question 6
+var email = "danishidrees0101@gmail.com";
+alert("My email address is " + email);
+
+
+//  Question 7
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+
+//  Question 8
+document.write('Yah! I can write HTML content through JavaScript' + '<br><br>')
+
+
+//  Question 9
+var special = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(special);
+document.write(special);
+
